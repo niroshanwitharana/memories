773d929cff2abdccd6d5a53f46e7d1abf3cfe491
niroshanwitharana <nrosh_uoc@yahoo.com>
2022-09-10 22:29:56 +0100

    schema and backend file structure setup

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router();
+import{getPost, createPost} from '../controllers/post.js'
 
-router.get('/', (req, res)=>{
-    res.send('THIS WORKS');
-})
+router.get('/', getPost)
+router.post('/', createPost)
 
 export default router;
